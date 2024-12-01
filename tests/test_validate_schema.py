241f6cae12818eb7cc468af75016c49169d287a8
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-12-01 09:13:12 +0100

    test: add sample test

---
+++ tests/test_validate_schema.py
@@ -0,0 +1,23 @@
+from typing import TypedDict
+
+import polars as pl
+
+from polars_validator import DataFrame, validate_schema
+
+
+def test_validate_schema():
+    class User(TypedDict):
+        id: int
+        name: str
+        email: str
+
+    @validate_schema(User)
+    def get_users_from_db() -> pl.DataFrame:
+        # Simulate database query
+        data = [
+            {"id": 1, "name": "Alice", "email": "alice@example.com"},
+            {"id": 2, "name": "Bob", "email": "bob@example.com"},
+        ]
+        return pl.DataFrame(data)
+
+    users: DataFrame[User] = get_users_from_db()
